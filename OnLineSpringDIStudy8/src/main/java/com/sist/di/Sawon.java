f40580e0213a76c56a5baf845f8aa65fdac7cbf6
Chihoon <achoon91@gmail.com>
2020-11-30 09:11:51 +0900

    Spring DI Study8

---
+++ OnLineSpringDIStudy8/src/main/java/com/sist/di/Sawon.java
@@ -0,0 +1,22 @@
+package com.sist.di;
+
+import org.springframework.stereotype.Component;
+
+@Component("sa")
+public class Sawon {
+	private String name;
+	private String sex;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+}
